6c03c571f00a6e1324fb53fa923ecf28209423a1
Lars Kåre Syversen <lars-kare.syversen@knowit.no>
2025-04-01 15:09:21 +0200

    fix datapath

--- sentinelprocessor/retile.py
+++ sentinelprocessor/retile.py
@@ -7,15 +7,15 @@ from tqdm import tqdm
 logger = logging.getLogger("retile")
 
 
-def retile_products(self, products, overlap=32):
+def retile_products(datapath, products, overlap=32):
     logger.debug("Retiling products with overlap %d" % overlap)
-    zipdir = f"{self.dataPath}/extracted"
+    zipdir = f"{datapath}/extracted"
     virts = []
 
     paths = []
     for id in products:
         product = products[id]
-        paths.append(f"{self.dataPath}/products/{product['title']}")
+        paths.append(f"{datapath}/products/{product['title']}")
 
     with tqdm(desc="Unzipping products",
               total=len(paths),
@@ -51,14 +51,14 @@ def retile_products(self, products, overlap=32):
     # os.system("gdalwarp -te %s %s %s %s virt.vrt virt2.vrt" % (s1[0], s1[1], s2[0], s2[1]))
 
     logger.info("Retiling images with tilesize %s" % tilesize)
-    os.mkdir(f"{self.dataPath}/retiled")
+    os.mkdir(f"{datapath}/retiled")
     os.system(
         'gdal_retile -ps %s %s -overlap %s -targetDir %s %s' % (
-            tilesize, tilesize, overlap, f"{self.dataPath}/retiled", "mosaic.vrt"))
+            tilesize, tilesize, overlap, f"{datapath}/retiled", "mosaic.vrt"))
 
-    directory = os.fsencode(f"{self.dataPath}/retiled")
+    directory = os.fsencode(f"{datapath}/retiled")
     dirlist = os.listdir(directory)
-    processeddir = f"{self.dataPath}/processed"
+    processeddir = f"{datapath}/processed"
     os.mkdir(processeddir)
 
     processpaths = []
@@ -68,7 +68,7 @@ def retile_products(self, products, overlap=32):
               unit="tile") as progress:
         for file in dirlist:
             filename = os.fsdecode(file)
-            filename_full = f"{self.dataPath}/retiled/{filename}"
+            filename_full = f"{datapath}/retiled/{filename}"
             file_hash = hashlib.md5(filename.encode()).hexdigest()
             os.system("gdal_translate -q -of COG %s %s" %
                       (filename_full, f"{processeddir}/{file_hash}.tif"))
