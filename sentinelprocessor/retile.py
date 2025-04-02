3c012d08ea1c77380e90c77e88dbcef85e0da405
Lars Kåre Syversen <lars-kare.syversen@knowit.no>
2025-04-02 12:47:11 +0200

    attempting to speed up retiling method in container

--- sentinelprocessor/retile.py
+++ sentinelprocessor/retile.py
@@ -35,23 +35,23 @@ def retile_products(datapath, products, overlap=32):
         for id in products:
             product = products[id]
             bandpaths = f"{zipdir}/{product['title']}"
-            virts.append(f"{bandpaths}/tile.vrt")
+            virts.append(f"{bandpaths}/tile.tif")
             os.system(f"gdalbuildvrt -q -separate %s %s %s %s %s"
                       % (f"{bandpaths}/rgb.vrt", f"{bandpaths}/B02.tif", f"{bandpaths}/B03.tif",
                          f"{bandpaths}/B04.tif", f"{bandpaths}/B08.tif"))
             os.system("gdalwarp -q -t_srs EPSG:3857 %s %s" %
-                      (f"{bandpaths}/rgb.vrt", f"{bandpaths}/tile.vrt"))
+                      (f"{bandpaths}/rgb.vrt", f"{bandpaths}/tile.tif"))
             progress.update()
 
     source_tiles = ' '.join(virts)
     tilesize = 10008 + overlap * 2
 
     logger.info("Building image mosaic VRT")
-    os.system("gdalbuildvrt mosaic.vrt %s" % source_tiles)
+    os.system("gdalbuildvrt %s %s" % (f"{datapath}/mosaic.vrt", source_tiles))
     # os.system("gdalwarp -te %s %s %s %s virt.vrt virt2.vrt" % (s1[0], s1[1], s2[0], s2[1]))
 
     logger.info("Retiling images with tilesize %s" % tilesize)
-    os.mkdir(f"{datapath}/retiled")
+    os.makedirs(f"{datapath}/retiled", exist_ok=True)
     os.system(
         'gdal_retile -ps %s %s -overlap %s -targetDir %s %s' % (
             tilesize, tilesize, overlap, f"{datapath}/retiled", "mosaic.vrt"))
